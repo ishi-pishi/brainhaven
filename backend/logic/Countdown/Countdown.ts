5f14853cc964f1bc64e811fa1291ef90e45f206b
Isha <i@kdau.ca>
2025-12-17 15:53:34 -0800

    countdown/feat: add countdown class

---
+++ backend/logic/Countdown/Countdown.ts
@@ -0,0 +1,18 @@
+import { Time } from "./Time";
+import { Session } from "./Session";
+
+/**
+ * Represents an ACTIVE session that is counting down, containing
+ * - Session we are completing
+ * - Total time elapsed so far
+ */
+export class Countdown {
+    private elapsed: Time;
+    private session: Session;
+
+    // Construct a new session with no elapsed time
+    constructor(session: Session) {
+        this.elapsed = new Time(0);
+        this.session = session;
+    }
+}
