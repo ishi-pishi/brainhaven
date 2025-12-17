a41a904c6b9437d1fc4b97b3daf4518def713d2c
Isha <i@kdau.ca>
2025-12-17 15:39:52 -0800

    feat: set up time class

---
+++ backend/logic/Time.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents a time duration in seconds.
+ */
+export class Time {
+    private seconds: number;
+
+    constructor(seconds: number) {
+        this.seconds = seconds;
+    }
+}
