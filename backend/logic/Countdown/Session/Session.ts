f21e2b0d873c647a6bc188d3a3c362ecdfc083b1
Isha <i@kdau.ca>
2025-12-17 16:03:38 -0800

    countdown/refactor: separate Session blocks into TimeBlocks

---
+++ backend/logic/Countdown/Session/Session.ts
@@ -0,0 +1,12 @@
+import { TimeBlock } from "./TimeBlock";
+
+/**
+ * Represents a focus session, containing:
+ * - duration of time spent in work
+ * - duration of time spent in break
+ * - number of blocks (each block containing a work followed by a break)
+ */
+export class Session {
+    private block: TimeBlock
+    private numBlocks: number;
+} 
---
+++ backend/logic/Countdown/Session/TimeBlock.ts
@@ -0,0 +1,8 @@
+import { Time } from "../Time";
+/**
+ * A class representing a block of time (either work or break)
+ */
+
+export class TimeBlock {
+    private duration: Time;
+}
