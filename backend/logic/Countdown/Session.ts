91fea353ca8608a7e53d14cff0ba2488f741f3b5
Isha <i@kdau.ca>
2025-12-17 17:41:57 -0800

    countdown/fix: fix blocks/cycles overlap

--- backend/logic/Countdown/Session.ts
+++ backend/logic/Countdown/Session.ts
@@ -4,19 +4,19 @@ import { Time } from "./Time";
  * Represents a focus session, containing:
  * - duration of time spent in work
  * - duration of time spent in break
- * - number of blocks (each block containing a work followed by a break)
+ * - number of cycles (each block containing a work followed by a break)
  */
 export class Session {
     private work: Time;
     private break: Time;
-    private numBlocks: number;
+    private numCycles: number;
 
     // Constructs session with work/break amounts with the given number of SECONDS
     // and numBlocks.
     // Important: work/break are in seconds NOT time.
-    constructor(workSeconds: number, breakSeconds: number, numBlocks: number) {
+    constructor(workSeconds: number, breakSeconds: number, numCycles: number) {
         this.work = new Time(workSeconds);
         this.break = new Time(breakSeconds);
-        this.numBlocks = numBlocks;
+        this.numCycles = numCycles;
     }
 } 
