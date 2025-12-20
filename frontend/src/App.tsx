6b1b4bc4ffe3fa8273f62a9009d2909a9129abbc
unknown <i@kdau.ca>
2025-12-19 18:02:15 -0800

    Add timer start/controller to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+
 import TimerDisplay from "./components/pomodoroSession/TimerDisplay";
+import { TimerManager } from "./components/pomodoroSession/TimerManager";
 
 export default function App() {
+  useEffect(() => {
+    TimerManager.getInstance().startTimerSeconds(10);
+  }, []);
+
   return (
     <div className="">
       <TimerDisplay />
