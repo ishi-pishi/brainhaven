09a3129276e6f2b4d8043cd084c09f2c6cc3a170
unknown <i@kdau.ca>
2025-12-19 17:40:24 -0800

    Remove temp timer start

--- frontend/src/components/pomodoroSession/TimerDisplay.tsx
+++ frontend/src/components/pomodoroSession/TimerDisplay.tsx
@@ -8,8 +8,6 @@ export default function TimerDisplay() {
   useEffect(() => {
     const unsubscribe = timerManager.addTickListener(setTimeLeftMs);
 
-    timerManager.startTimerSeconds(10);
-
     return () => {
       unsubscribe();
     };
