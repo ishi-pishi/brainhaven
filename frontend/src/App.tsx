79a499cfa5298de0536413cf094524ba69a18f87
unknown <i@kdau.ca>
2025-12-20 18:46:34 -0800

    Reset app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from "react";
-
-import TimerDisplay from "./components/pomodoroSession/TimerDisplay";
-import { ActiveSession } from "./logic/timer/ActiveSession";
-import { SessionSettings } from "./logic/timer/SessionSettings";
-
 export default function App() {
-  useEffect(() => {
-    const settings = new SessionSettings(10*1000, 5*1000, 2);
-    const session = new ActiveSession(settings);
-    session.start(); // starts the first block internally
-  }, []);
-  
   return (
     <div className="">
-      <TimerDisplay />
+      Hi
     </div>
   );
 }
