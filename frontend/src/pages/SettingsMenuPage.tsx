6563b7053c371a07914fbe551d8a182e6965d568
unknown <i@kdau.ca>
2025-12-21 16:28:45 -0800

    Add colors

--- frontend/src/pages/SettingsMenuPage.tsx
+++ frontend/src/pages/SettingsMenuPage.tsx
@@ -2,7 +2,7 @@ import { SettingsMenu } from "../components/setSettings/SettingsMenu";
 
 export function SettingsMenuPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen">
       <SettingsMenu />
     </div>
   );
