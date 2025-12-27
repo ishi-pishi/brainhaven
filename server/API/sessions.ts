9c427be3904694cf09cf855a11ec0164f23c529f
unknown <i@kdau.ca>
2025-12-26 20:08:38 -0800

    Add routing with stubs for handlers

---
+++ server/API/sessions.ts
@@ -0,0 +1,30 @@
+import express from "express"
+
+const app = express();
+const port = 3000;
+
+
+// GET /sessions/:id
+app.get('/sessions/:id', (req, res) => {
+    // TODO
+});
+
+// GET /sessions
+app.get('/sessions', (req, res) => {
+    // TODO
+});
+
+// POST /sessions
+app.post('/sessions', (req, res) => {
+    // TODO
+});
+
+// PUT /sessions/:id
+app.put('/sessions/:id', (req, res) => {
+    // TODO
+});
+
+// DELETE /sessions/:id
+app.delete('/sessions/:id', (req, res) => {
+    // TODO
+});
