b37d0ad4f827f1bfe5b0b2ea3b4b9c88d20163fe
unknown <i@kdau.ca>
2025-12-27 12:26:05 -0800

    Add DBSessionManager class and stubs

--- server/SessionManager/DBSessionManager.ts
+++ server/SessionManager/DBSessionManager.ts
@@ -1,1 +1,18 @@
 import Database from 'better-sqlite3';
+
+import { ISessionManager, NotFoundError, InvalidSessionError, SessionManagerError } from './ISessionManager';
+import { SessionData, SessionMap } from '../../shared/Types';
+
+class DBSessionManager implements ISessionManager {
+    loadSessions(): Promise<SessionMap> {
+        throw new Error("This puppy ain't implemented yet!");
+    }
+
+    loadSessionById(id: string): Promise<SessionData> {
+        throw new Error("This puppy ain't implemented yet!");
+    }
+
+    saveSession(session: SessionData): Promise<void> {
+        throw new Error("This puppy ain't implemented yet!");
+    }
+}
