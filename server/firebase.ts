97819b05dda81f9f6c55ed605f7b59ee6f6c8755
unknown <i@kdau.ca>
2025-12-28 14:52:06 -0800

    Initialize firbase

---
+++ server/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkAMDZSC8p0FbxcIjR2tRF-acpelZiEJY",
+  authDomain: "brainhaven.firebaseapp.com",
+  projectId: "brainhaven",
+  storageBucket: "brainhaven.firebasestorage.app",
+  messagingSenderId: "582041622284",
+  appId: "1:582041622284:web:1ca675ecb230cfcc1b5b29",
+  measurementId: "G-WZ0P1G7DL4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
