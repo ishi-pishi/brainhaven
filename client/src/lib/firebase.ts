5887387ae85887d7a048a8a433fb137bfce62d88
unknown <i@kdau.ca>
2026-01-08 23:22:42 -0800

    Switch firebase project

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -10,14 +10,15 @@ import { connectorConfig } from '@dataconnect/generated';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkAMDZSC8p0FbxcIjR2tRF-acpelZiEJY",
-  authDomain: "brainhaven.firebaseapp.com",
-  projectId: "brainhaven",
-  storageBucket: "brainhaven.firebasestorage.app",
-  messagingSenderId: "582041622284",
-  appId: "1:582041622284:web:1ca675ecb230cfcc1b5b29",
-  measurementId: "G-WZ0P1G7DL4"
+  apiKey: "AIzaSyDo0ZbSyQvkAKVTuTXUbW2dYBZzZ5aV_5U",
+  authDomain: "brainhaven2.firebaseapp.com",
+  projectId: "brainhaven2",
+  storageBucket: "brainhaven2.firebasestorage.app",
+  messagingSenderId: "603797853502",
+  appId: "1:603797853502:web:6490a3c501647afb01086f",
+  measurementId: "G-JS2RG85EYN"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
