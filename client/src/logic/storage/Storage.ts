c4f073bb356247815a7dc7aed8d0393a92cabb36
unknown <i@kdau.ca>
2026-01-15 15:04:41 -0800

    Add user and subject storage methods

--- client/src/logic/storage/Storage.ts
+++ client/src/logic/storage/Storage.ts
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createUser, createStudySession } from "@dataconnect/generated";
+import { createUser, createStudySession, mySubjects, createSubject } from "@dataconnect/generated";
 import { auth } from "@/lib/firebase";
 import type { StudySession } from "./Types";
 
@@ -9,7 +9,6 @@ export async function signupWithEmail(email: string, password: string) {
     await createUserWithEmailAndPassword(auth, email, password);
     await createUser({ displayName: "placeholder display name" });
 }
-
 
 // Saves a completed study session to the database.
 export async function saveSession(session: StudySession) {
@@ -24,4 +23,26 @@ export async function saveSession(session: StudySession) {
         productivityRating: session.productivityRating ?? null,
         reflections: session.reflections ?? null,
     });
+}
+
+// Saves a subject to the database
+export async function saveSubject(name: string) {
+    if (await subjectNameAlreadyExists(name)) {
+        throw new Error("Subject name already exists");
+    }
+
+    return await createSubject({ name });
+}
+
+// Returns a list of the names of the user's subjects in the database
+export async function getSubjectNames() {
+    const { data } = await mySubjects();
+    return data.subjects.map(subject => subject.name);
+}
+
+// Checks if a subject name already exists in the database
+// Returns true if it does, false if it doesn't
+async function subjectNameAlreadyExists(name: string) {
+    const { data } = await mySubjects();
+    return !data.subjects.some(subject => subject.name.toLowerCase() === name.toLowerCase());
 }
