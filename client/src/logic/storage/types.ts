000241f7541ea418b5b703f5db341795a3d94b44
unknown <i@kdau.ca>
2026-02-18 23:01:02 -0800

    Separated StudySession into StudySession and FinishedTimer types

--- client/src/logic/storage/types.ts
+++ client/src/logic/storage/types.ts
@@ -1,10 +1,14 @@
-export type StudySession = {
+export type FinishedTimer = {
   subjectId: string
   startTime: string
   endTime: string
   workBlockMs: number
   breakBlockMs: number
   intendedCycles?: number
+}
+
+export type StudySession = {
+  timer: FinishedTimer
   productivityRating?: number
   reflections?: string
   earnedCurrency: number
