962ea1a2f6d68b2147f13e814b95392004adf04c
unknown <i@kdau.ca>
2026-02-21 14:41:53 -0800

    Add reload warning

--- client/src/pages/App/TimerPage.tsx
+++ client/src/pages/App/TimerPage.tsx
@@ -4,6 +4,8 @@ import { useDisableScroll } from "@/hooks/smallHooks";
 import { ActiveSession } from "@/logic/timer/ActiveSession";
 import { TimerSection } from "@/components/Timer/TimerSection";
 
+
+
 /**
  *  This component is a page containing all the timer componets.
  */
@@ -12,6 +14,18 @@ export function TimerPage() {
   const session = ActiveSession.getInstance();
   useDisableScroll();
 
+
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (session && !session.isFinished()) {
+        e.preventDefault();
+        e.returnValue = ""; // It's deprecated, but I don't know how else to do it
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => window.removeEventListener("beforeunload", handleBeforeUnload);
+  }, [session])
 
   useEffect(() => {
     if (!session) return;
